8a276f692a1155361c3ea13e7f2de029fb8965ba
msimmainofficial <msimmainofficial@gmail.com>
2026-07-30 23:10:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,15 @@
+/* ==========================================
+   MSIM APP v2
+   Supabase Configuration
+========================================== */
 
+const SUPABASE_URL = "https://cmtttmuiwgwmdqgcztgc.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_LEJNJGx5T1BHk9JpnRD8Qg_0j1O_enx";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
+
+console.log("Supabase Connected");
