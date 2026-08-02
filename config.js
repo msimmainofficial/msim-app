2c87da9c8f965f18f36964cdb5068bbb905814c2
msimmainofficial <msimmainofficial@gmail.com>
2026-08-02 21:14:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,13 +3,27 @@
    Supabase Configuration
 ========================================== */
 
-const SUPABASE_URL = "https://cmtttmuiwgwmdqgcztgc.supabase.co";
+const SUPABASE_URL =
+"https://cmtttmuiwgwmdqgcztgc.supabase.co";
 
-const SUPABASE_KEY = "sb_publishable_LEJNJGx5T1BHk9JpnRD8Qg_0j1O_enx";
+const SUPABASE_ANON_KEY =
+"sb_publishable_LEJNJGx5T1BHk9JpnRD8Qg_0j1O_enx";
 
-const supabase = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
+/* Supabase CDN */
+const supabaseScript = document.createElement("script");
+supabaseScript.src =
+"https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2";
+
+supabaseScript.onload = () => {
+
+window.supabase =
+supabase.createClient(
+SUPABASE_URL,
+SUPABASE_ANON_KEY
 );
 
-console.log("Supabase Connected");
+console.log("✅ Supabase Connected");
+
+};
+
+document.head.appendChild(supabaseScript);
